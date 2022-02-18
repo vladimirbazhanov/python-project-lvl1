8c8dc24ce04b349fb67bff1da0e3a9f49fdfe1cd
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2022-02-18 17:02:17 +0300

    Вернул обратно файл cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,9 @@
-from brain_games.shared import welcome_user, get_username, greet_user
+from brain_games.cli import welcome_user
 
 
 def main():
+    print('Welcome to the Brain Games!')
     welcome_user()
-    username = get_username()
-    greet_user(username)
 
 
 if __name__ == '__main__':
